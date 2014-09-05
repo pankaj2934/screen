93c51a2f3b10f4c9dce9a6c0210538ab4b77b790
root <root@lezqdnasv34.staples.com>
2014-09-05 17:52:44 -0400

    inital working screen install

---
+++ manifests/init.pp
@@ -0,0 +1,5 @@
+#
+
+	class screen {
+		include screen::install 
+	}
---
+++ manifests/install.pp
@@ -0,0 +1,43 @@
+
+
+class screen::install 
+       (
+		$version = '4.2.1',
+		$url	 = 'wget http://ftp.gnu.org/gnu/screen/screen-4.2.1.tar.gz -O /tmp/screen.gz',
+	)
+{
+	
+	#########################################
+	#
+	#  install screen and use files from puppet files
+	#
+	#########################################
+	
+	package {'ncurses-devel':
+		ensure 		=> present,
+		}
+
+	exec {'screen-download':
+		command		=> $url,
+		cwd		=> '/tmp',
+		unless 		=>'which pk',
+		path		=> [ '/bin' , '/usr/bin' ],
+		require 	=> Package['ncurses-devel'],
+	     }
+	
+	exec { 'screen-unzip':
+		command		=> 'tar -zxf /tmp/screen.gz',
+		cwd		=> '/tmp',
+		unless		=> 'which pk',
+		path		=> [ '/bin' , '/usr/bin' ],
+		require		=> Exec['screen-download'],
+	      }	
+		
+	exec { 'screen-configure':
+		command		=> '/tmp/screen-4.2.1/configure && make /tmp/screen-4.2.1/install && /tmp/screen-4.2.1/install -m 644 etc/etcscreenrc /etc/screenrc ',
+		cwd		=>'/tmp/screen-4.2.1',
+		unless		=> 'which pk',
+		path		=> [ '/bin' , '/usr/bin' ],
+		require		=> Exec['screen-unzip'],
+	      }	
+}
